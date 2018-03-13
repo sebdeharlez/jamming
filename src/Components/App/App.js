a6021d84acdef6e78f217549ca8d658f1cc98365
de Harlez de Deulin (de Harlez) <sebastien.deHarlez@thomsonreuters.com>
2018-03-13 18:47:15 +0100

    change jammming to jamming in app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -64,7 +64,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="Highlight">mmm</span>ing</h1>
+        <h1>Ja<span className="Highlight">mm</span>ing</h1>
         <div className="App">
           <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
